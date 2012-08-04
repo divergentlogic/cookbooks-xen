4ee43da9afe703eda1cfbbaf47460eb69403fe0c
Ceaser Larry <clarry@divergentlogic.com>
2012-08-04 00:42:30 -0600

    Updating license and copyright info

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,20 @@
+#
+# Cookbook Name:: xen
+#
+# Copyright 2012, Divergent Logic, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # It is normally much better to create the bridge yourself in
 # /etc/network/interfaces.  network-bridge start does nothing if you
 # already have a bridge, and network-bridge stop does nothing if the
